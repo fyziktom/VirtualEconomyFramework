556841f2d41d8cecd39c5416300c1c8ae6d7f772
fyziktom <tomas.svoboda@technicinsider.com>
2021-05-28 21:14:39 +0200

    add basic UI for Doge Account in VENFT App

--- VirtualEconomyFramework/VENFTApp-Blazor/Services/AppData.cs
+++ VirtualEconomyFramework/VENFTApp-Blazor/Services/AppData.cs
@@ -7,6 +7,7 @@ using VEDriversLite.Bookmarks;
 public class AppData
 {
     public NeblioAccount Account { get; set; } = new NeblioAccount();
+    public DogeAccount DogeAccount { get; set; } = new DogeAccount();
     public List<TokenOwnerDto> VENFTOwners = new List<TokenOwnerDto>();
 }
 
