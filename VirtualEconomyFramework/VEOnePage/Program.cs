f82f04fcaabff6d783b985564eee7fa2391343ad
fyziktom <tomas.svoboda@technicinsider.com>
2022-02-04 10:46:34 -0400

    add AppData service, add dependencies

--- VirtualEconomyFramework/VEOnePage/Program.cs
+++ VirtualEconomyFramework/VEOnePage/Program.cs
@@ -1,3 +1,4 @@
+using Coruzant.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace VEOnePage
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<AppData>();
 
             await builder.Build().RunAsync();
         }
