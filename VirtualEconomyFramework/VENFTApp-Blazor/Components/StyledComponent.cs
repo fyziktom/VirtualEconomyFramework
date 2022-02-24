6c56b6c6c5a4cf586af56e1cbf0b3352fc4411ed
Štěpán Wünsch <sw@lowenware.com>
2022-02-24 10:22:14 +0100

    Implement Modal component prototype

---
+++ VirtualEconomyFramework/VENFTApp-Blazor/Components/StyledComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public abstract class StyledComponent : ComponentBase
+{
+    [Parameter]
+    public string Style { get; set; }
+    [Parameter]
+    public string Class { get; set; }
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+}
+
+public abstract class StyledComponent<T> : ComponentBase
+{
+    [Parameter]
+    public string Style { get; set; }
+    [Parameter]
+    public string Class { get; set; }
+    [Parameter]
+    public RenderFragment<T> ChildContent { get; set; }
+}
