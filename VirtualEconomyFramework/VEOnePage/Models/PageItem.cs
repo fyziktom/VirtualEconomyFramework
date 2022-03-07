88e99067aa23e584a08ca43a13ec66b4a24f1595
Fh Ndiritu <fhndiritu@gmail.com>
2022-03-07 21:46:01 +0300

    Add pagination

---
+++ VirtualEconomyFramework/VEOnePage/Models/PageItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Coruzant.Models
+{
+    public class PageItem
+    {
+        public string Text { get; set; }
+        public int PageIndex { get; set; }
+        public bool Enabled { get; set; }
+        public bool Active { get; set; }
+
+        public PageItem(int pageIndex, bool enabled, string text)
+        {
+            this.PageIndex = pageIndex;
+            this.Enabled = enabled;
+            this.Text = text;
+        }
+    }
+}
