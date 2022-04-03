d4984ff16039b3a0163223c16b8854fa56a40b96
fyziktom <tomas.svoboda@technicinsider.com>
2022-04-03 01:37:52 -0400

    Add base for BaseDataPlace WASM App

---
+++ VirtualEconomyFramework/BDP-WASM/Program.cs
@@ -0,0 +1,27 @@
+using BDP_WASM;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
+builder.Services.AddSingleton<AppData>();
+
+await builder.Build().RunAsync();
---
+++ VirtualEconomyFramework/BDP-WASM/Services/AppData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VEDriversLite;
+using VEDriversLite.NFT.Imaging.Xray.Dto;
+using VEDriversLite.Bookmarks;
+
+public enum MintingToolbarActionType
+{
+    None,
+    PreviousStep,
+    NextStep,
+    Save,
+    Finish,
+    Mint,
+    Cancel,
+    ClearAll,
+    ClearActualForm
+}
+public class MintingToolbarActionDto
+{
+    public MintingToolbarActionType Type { get; set; }
+    public string[] Args { get; set; }
+}
+public class AppData
+{
+    public NeblioAccount Account { get; set; } = new NeblioAccount();
+    public bool IsAccountLoaded { get; set; } = false;
+    public Dictionary<string,XrayExposureParameters> ExposureParametersTemplates { get; set; } = new Dictionary<string, XrayExposureParameters>();
+    public Dictionary<string, DetectorDataDto> DetectorParametersTemplates { get; set; } = new Dictionary<string, DetectorDataDto>();
+}
+
