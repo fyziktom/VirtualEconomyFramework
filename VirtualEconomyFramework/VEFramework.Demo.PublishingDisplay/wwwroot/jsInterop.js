ea1664d2be3940fa0ed0116a6e61156c928c1981
fyziktom <tomas.svoboda@technicinsider.com>
2022-06-12 14:16:36 -0500

    add podcasts and about-fellows page

---
+++ VirtualEconomyFramework/VEFramework.Demo.PublishingDisplay/wwwroot/jsInterop.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".text", function () {
+        $(this).find("a").attr('target', '_blank');
+    });
+});
+
+window.jsFunctions = {
+    buzzsproutPodcast: function (link) {
+        var script = document.createElement('script');
+        script.src = link;
+        document.head.appendChild(script);
+    }
+}
