bd5fabbe7923e7f22b8e6a5b1d7df4351c620e1d
fyziktom <tomas.svoboda@technicinsider.com>
2022-06-17 23:27:26 -0500

    WIP: added basic API server for publishing Demo

---
+++ VirtualEconomyFramework/VEBlazor.Demo.Publishing.Server/Controllers/NFTController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VEBlazor.Demo.Publishing.Server.Services;
+using VEDriversLite.NFT;
+using VEDriversLite.NFT.Coruzant;
+using VEDriversLite.NFT.Tags;
+
+namespace VEBlazor.Demo.Publishing.Server.Controllers
+{
+    public class NFTController : Controller
+    {
+        // GET: NFTController/GetNFT/{Utxo}
+        [HttpGet("GetNFT/{utxo}")]        
+        public async Task<INFT> GetNFT(string utxo)
+        {
+            return await Services.DataCoreService.GetNFT(utxo);
+        }
+
+        [HttpGet("GetNFTArticles")]
+        public async Task<Dictionary<string, INFT>> GetNFTArticles()
+        {
+            return await GetNFTArticles(0);
+        }        
+        [HttpGet("GetNFTArticles/{settings}/{skip}/{take}")]
+        public async Task<Dictionary<string, INFT>> GetNFTArticles(int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+                           
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantArticle).Skip(skip).Take(take))
+                {
+                    nft.TxDetails = null;
+                    dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                }
+            }
+            return dict;
+        }
+
+        [HttpGet("GetNFTProfiles")]
+        public async Task<Dictionary<string, INFT>> GetNFTProfiles()
+        {
+            return await GetNFTProfiles(0);
+        }        
+        [HttpGet("GetNFTProfiles/{settings}/{skip}/{take}")]        
+        public async Task<Dictionary<string, INFT>> GetNFTProfiles(int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantProfile).Skip(skip).Take(take))
+                {
+                    nft.TxDetails = null;
+                    dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                }
+            }
+            return dict;
+        }
+
+        [HttpGet("GetNFTPodcasts")]
+        public async Task<Dictionary<string, INFT>> GetNFTPodcasts()
+        {
+            return await GetNFTPodcasts(0);
+        }
+        [HttpGet("GetNFTPodcasts/{settings}/{skip}/{take}")]
+        public async Task<Dictionary<string, INFT>> GetNFTPodcasts(int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantProfile).Skip(skip).Take(take))
+                {
+                    if (!string.IsNullOrEmpty((nft as CoruzantProfileNFT)?.PodcastId))
+                    {
+                        nft.TxDetails = null;
+                        dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                    }
+                }
+            }
+            return dict;
+        }        
+
+        [HttpGet("GetNFTTags")]
+        public async Task<Dictionary<string, Tag>> GetNFTTags()
+        {
+            var dict = new Dictionary<string, Tag>();
+            var tags = NFTDataContext.Tags.Values.ToList()?.OrderByDescending(t => t.Count).ToList();
+            if (tags == null) return dict;
+            
+            foreach (var tag in tags)
+                dict.Add(tag.Name, tag);
+            return dict;      
+        }
+        [HttpGet("GetNFTTags/{skip}/{take}")]
+        public async Task<Dictionary<string, Tag>> GetNFTTags(int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, Tag>();
+            var tags = NFTDataContext.Tags.Values.ToList()?.OrderByDescending(t => t.Count).ToList().Skip(skip).Take(take);
+            if (tags == null) return dict;
+
+            foreach (var tag in tags)
+                dict.Add(tag.Name, tag);
+            return dict;
+        }
+
+        [HttpGet("GetNFTArticlesByTags/{tagName}")]
+        public async Task<Dictionary<string, INFT>> GetNFTArticlesByTags(string tagName)
+        {
+            return await GetNFTArticlesByTags(tagName, 0);
+        }
+        [HttpGet("GetNFTArticlesByTags/{tagName}/{settings}/{skip}/{take}")]
+        public async Task<Dictionary<string, INFT>> GetNFTArticlesByTags(string tagName, int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantArticle).Where(n => n.TagsList.Contains(tagName)).Skip(skip).Take(take))
+                {
+                    nft.TxDetails = null;
+                    dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                }                    
+            }            
+
+            return dict;
+        }
+
+        [HttpGet("GetNFTProfilessByTags/{tagName}")]
+        public async Task<Dictionary<string, INFT>> GetNFTProfilesByTags(string tagName)
+        {
+            return await GetNFTProfilesByTags(tagName, 0);
+        }
+        [HttpGet("GetNFTProfilessByTags/{tagName}/{settings}/{skip}/{take}")]
+        public async Task<Dictionary<string, INFT>> GetNFTProfilesByTags(string tagName, int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantProfile).Where(n => n.TagsList.Contains(tagName)).Skip(skip).Take(take))
+                {
+                    nft.TxDetails = null;
+                    dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                }                    
+            }
+
+            return dict;
+        }
+
+        [HttpGet("GetNFTPodcastsByTags/{tagName}")]
+        public async Task<Dictionary<string, INFT>> GetNFTPodcastsByTags(string tagName)
+        {
+            return await GetNFTPodcastsByTags(tagName, 0);
+        }
+        [HttpGet("GetNFTPodcastsByTags/{tagName}/{settings}/{skip}/{take}")]
+        public async Task<Dictionary<string, INFT>> GetNFTPodcastsByTags(string tagName, int settings = 0, int skip = 0, int take = 25)
+        {
+            var dict = new Dictionary<string, INFT>();
+
+            if (MainDataContext.ObservedAccountsTabs.TryGetValue(MainDataContext.MainCoruzantPublishingAddress, out var tab))
+            {
+                foreach (var nft in tab.NFTs.Where(n => n.Type == NFTTypes.CoruzantProfile).Where(n => n.TagsList.Contains(tagName)).Skip(skip).Take(take))
+                {
+                    if (!string.IsNullOrEmpty((nft as CoruzantProfileNFT).PodcastId))
+                    {
+                        nft.TxDetails = null;
+                        dict.Add($"{nft.Utxo}:{nft.UtxoIndex}", nft);
+                    }
+                }
+            }
+
+            return dict;
+        }
+
+    }
+}
---
+++ VirtualEconomyFramework/VEBlazor.Demo.Publishing.Server/CoruzantCoreBackgroundService.cs
@@ -0,0 +1,81 @@
+﻿using VEBlazor.Demo.Publishing.Server.Services;
+using VEDriversLite.NFT;
+
+namespace VEBlazor.Demo.Publishing.Server.WindowsService;
+
+public sealed class CoruzantCoreBackgroundService : BackgroundService
+{
+    private IConfiguration _settings;
+    private readonly DataCoreService _dataCoreService;
+    private readonly ILogger<CoruzantCoreBackgroundService> _logger;
+
+    public CoruzantCoreBackgroundService(
+        IConfiguration settings,
+        DataCoreService jokeService,
+        ILogger<CoruzantCoreBackgroundService> logger) =>
+        (_settings, _dataCoreService, _logger) = (settings, jokeService, logger);        
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        
+        try
+        {
+            MainDataContext.IpfsSecret = _settings.GetValue<string>("IpfsSecret", string.Empty);
+            MainDataContext.IpfsProjectID = _settings.GetValue<string>("IpfsProjectID", string.Empty);
+            if (!string.IsNullOrEmpty(MainDataContext.IpfsSecret) && !string.IsNullOrEmpty(MainDataContext.IpfsProjectID))
+                NFTHelpers.LoadConnectionInfo(MainDataContext.IpfsProjectID, MainDataContext.IpfsSecret);
+
+            var acc = new List<string>();
+            _settings.GetSection("ObservedAccounts").Bind(acc);
+            if (acc != null)
+                MainDataContext.ObservedAccounts = acc;
+
+            Console.WriteLine("Loading Observing addresses...");
+            // load observed addresses
+            if (MainDataContext.ObservedAccounts.Count > 0)
+            {
+                for (var i = 0; i < MainDataContext.ObservedAccounts.Count; i++)
+                {
+                    try
+                    {
+                        var tab = new VEDriversLite.Bookmarks.ActiveTab(MainDataContext.ObservedAccounts[i]);
+                        tab.MaxLoadedNFTItems = 1500;
+                        await tab.StartRefreshing(10000, false, true);
+                        MainDataContext.ObservedAccountsTabs.TryAdd(tab.Address, tab);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Cannot start observing of account: " + MainDataContext.ObservedAccounts[i]);
+                    }
+                }
+            }
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                
+                /*                
+                var nft = await _dataCoreService.GetNFT("feff3736186c6d0c7d1412c8d8d1fe59367e9a91797ce40c4f7bd2bad8a046e0");
+                if (nft != null)
+                    _logger.LogWarning("{Name}", nft.Name);
+                else
+                    _logger.LogError("Cannot load the NFT.");
+                */
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{Message}", ex.Message);
+
+            // Terminates this process and returns an exit code to the operating system.
+            // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+            // performs one of two scenarios:
+            // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+            // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+            //
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+            Environment.Exit(1);
+        }
+    }
+}
