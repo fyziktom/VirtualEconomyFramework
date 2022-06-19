5638f160f4887bd9431188b491d077566154d2ce
fyziktom <tomas.svoboda@technicinsider.com>
2022-06-18 19:51:10 -0500

    add copy button to publishing display, fix bug in fellowslist

--- VirtualEconomyFramework/VEFramework.Demo.PublishingDisplay/wwwroot/jsInterop.js
+++ VirtualEconomyFramework/VEFramework.Demo.PublishingDisplay/wwwroot/jsInterop.js
@@ -3,5 +3,8 @@
         var script = document.createElement('script');
         script.src = link;
         document.head.appendChild(script);
+    },
+    copyToClipboard(text) {
+        navigator.clipboard.writeText(text);
     }
 }
