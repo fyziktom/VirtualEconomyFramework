8bbf36cf95936196d864152ef81012c0e8afb098
fyziktom <tomas.svoboda@technicinsider.com>
2023-03-12 11:37:09 -0400

    add demo for Blazor Server App which uses AI and NFTs to build dataset

---
+++ VirtualEconomyFramework/VEBlazor.Demo.AI.MintFreeForAI/MainDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VEDriversLite;
+using VEDriversLite.Dto;
+using VEDriversLite.Bookmarks;
+using VEDriversLite.NFT;
+using VEDriversLite.NeblioAPI;
+using VEDriversLite.AI.OpenAI;
+
+namespace VEBlazor.Demo.AI.MintFreeForAI
+{
+    public static class MainDataContext
+    {
+        public static ConcurrentDictionary<string, INFT> MintedNFT = new ConcurrentDictionary<string, INFT>();
+
+        public static string MainAccount { get; set; } = string.Empty;
+        public static VirtualAssistant? Assistant { get; set; } = null;
+    }
+}
