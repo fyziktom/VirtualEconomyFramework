23425f239fff16535be6c7c72ae59257a7e2dc63
fyziktom <tomas.svoboda@technicinsider.com>
2023-05-24 18:19:16 -0400

    add installation steps/script for Blockchain Indexer Server

---
+++ VirtualEconomyFramework/VEFramework.BlockchainIndexerServer/Resources/vef-bis-installation-script.sh
@@ -0,0 +1,28 @@
+﻿# VEFramework.BlockchainIndexerServer Linuxu installation and run script
+
+# Download the .NET 7.0 SDK
+echo "Downloading .NET 7.0 SDK..."
+wget https://download.visualstudio.microsoft.com/download/pr/351400ef-f2e6-4ee7-9d1b-4c246231a065/9f7826270fb36ada1bdb9e14bc8b5123/dotnet-sdk-7.0.302-linux-x64.tar.gz
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-7.0.302-linux-x64.tar.gz -C $HOME/dotnet
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+dotnet --info
+
+# Clone the repository
+echo "Cloning the repository"
+cd $HOME/vef
+git clone https://github.com/fyziktom/VirtualEconomyFramework.git
+
+# Checkout to blockchain indexer branch
+git checkout 182-blockchain-indexer
+
+# Go to the Blockchain Indexer Server folder
+cd $HOME/vef/VirtualEconomyFramework/VirtualEconomyFramework/VEFramework.BlockchainIndexerServer/
+
+# Optional Step: Edit appsettings.json if you use specific RPC Nebliod parameters
+
+# Publish server
+dotnet publish --configuration Release --output ./publish
+
+# Run server
+dotnet ./publish/VEFramework.BlockchainIndexerServer.dll
